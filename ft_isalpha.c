2ff7b5af6ae7620aa5e8ac53bbd063521fa6aca3
eestelle <yosiky@list.ru>
2021-10-01 16:04:53 +0300

    Add function ft_isalpha

---
+++ ft_isalpha.c
@@ -0,0 +1,15 @@
+static	int	ft_isupper(int c)
+{
+	return ('A' <= c && c <= 'Z');
+}
+
+static	int	ft_islower(int c)
+{
+	return ('a' <= c && c <= 'z');
+}
+
+int	ft_isalpha(int c)
+{
+	return (ft_isupper(c) || ft_islower(c) ? 1024 : 0);
+}
+
