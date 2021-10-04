399d7c74463e1f7c0d1bc9f04d83a00b51676e73
eestelle <eestelle@am-c2.msk.21-school.ru>
2021-10-04 18:32:43 +0300

    Add funciton ft_bzero
    	ft_bzero: write '\0' in bytes
    	test_funcc: uses ft_bzero

--- test_func.c
+++ test_func.c
@@ -2,17 +2,17 @@
 #include <string.h>
 
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 int	main(void)
 {
 	const int	size = 15;
 	char		str[size];
 
-	memset(str, 0, size);
-	for (int i = 0; i < size; i++)
+	for (int i = size - 1; i >= 0; i--)
 	{
-		memset(str, 0, i);
-		printf("%d. \tstr = '%s'\n", i, str); 
+		ft_bzero(str, i + 1);
+		printf("%d. \tstr = '%s'\n", i, str);
 		ft_memset(str, 'c', i);
 		printf("\tstr = '%s'\n", str);
 	}
