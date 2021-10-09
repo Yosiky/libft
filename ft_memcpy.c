d58ff650934e221f9610a5aaa3404ef746fffdb4
eestelle <yosiky@list.ru>
2021-10-09 18:07:52 +0300

    Optimized function is ft_memcpy"

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,9 +12,52 @@
 
 #include <stddef.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+typedef	unsigned long long	t_ull;
+
+t_ull	*ft_completion(t_ull *dest, t_ull **src, size_t len)
 {
-	while (n--)
-		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
-	return (dst);
+	while (len--)
+	{
+		dest[0] = (*src)[0]; 
+		dest[1] = (*src)[1]; 
+		dest[2] = (*src)[2]; 
+		dest[3] = (*src)[3]; 
+		dest[4] = (*src)[4]; 
+		dest[5] = (*src)[5]; 
+		dest[6] = (*src)[6]; 
+		dest[7] = (*src)[7]; 
+		dest += 8;
+		*src += 8;
+	}
+	return (dest);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	t_ull	*ptr_dest;
+	t_ull	*ptr_src;
+	char	*bptr_dest;
+	char	*bptr_src;
+	size_t	len;
+
+	if (dest == src || n == 0)
+		return (dest);
+	bptr_dest = (char *)dest;
+	bptr_src = (char *)src;
+	while ((size_t)bptr_dest % 8 && n--)
+		*(bptr_dest++) = *(bptr_src++);
+	ptr_dest = (t_ull *)bptr_dest;
+	ptr_src = (t_ull *)src;
+	len = (n) / 64;
+	if (len)
+		ptr_dest = ft_completion(ptr_dest, &ptr_src, len); 
+	len = (n % 64) / 8;
+	while (len--)
+		*(ptr_dest++) = *(ptr_src++);
+	bptr_dest = (char *)ptr_dest;
+	bptr_src = (char *)ptr_src;
+	len = n % 8;
+	while (len--)
+		*(bptr_dest++) = *(bptr_src++);
+	return (dest);
 }
