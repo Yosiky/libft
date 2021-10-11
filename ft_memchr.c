dff7e7306e30f0c2fd79ca46ca911c09272c90de
eestelle <yosiky@list.ru>
2021-10-11 18:50:06 +0300

    Need choose version

--- ft_memchr.c
+++ ft_memchr.c
@@ -12,13 +12,52 @@
 
 #include <stddef.h>
 
+typedef	unsigned long long	t_ull;
+
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while (n--)
+	char	*bptr;
+	size_t	len;
+	
+	bptr = (char *)s;
+	while ((size_t)bptr % 8 && n--)
+		if (*(bptr++) == (char)c)
+			return ((void *)bptr - 1);
+	len = (++n) / 8;
+	while (len--)
 	{
-		if (*((char *)s) == c)
-			return ((void *)s);
-		s++;
+		if (bptr[0] == (char)c)
+			return ((void *)bptr);
+		else if (bptr[1] == (char)c)
+			return ((void *)bptr + 1);
+		else if (bptr[2] == (char)c)
+			return ((void *)bptr + 2);
+		else if (bptr[3] == (char)c)
+			return ((void *)bptr + 3);
+		else if (bptr[4] == (char)c)
+			return ((void *)bptr + 4);
+		else if (bptr[5] == (char)c)
+			return ((void *)bptr + 5);
+		else if (bptr[6] == (char)c)
+			return ((void *)bptr + 6);
+		else if (bptr[7] == (char)c)
+			return ((void *)bptr + 7);
+		bptr += 8;
 	}
+	len = n % 8;
+	while ((size_t)bptr % 8 && n--)
+		if (*(bptr++) == (char)c)
+			return ((void *)bptr - 1);
 	return ((void *)0);
+	/*
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (n--)
+		if (*(ptr) == c)
+			return ((void *)ptr);
+		else
+			ptr++;
+	return ((void *)0);
+	*/
 }
