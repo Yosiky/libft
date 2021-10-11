5f96b73a807209ed440b425164bad997cc73134b
eestelle <yosiky@list.ru>
2021-10-11 19:32:24 +0300

    Need to think how optimize

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -12,6 +12,8 @@
 
 #include <stddef.h>
 
+typedef	unsigned long long	t_ull;
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (n-- && *s1 == *s2 && *s1 != '\0')
@@ -19,5 +21,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	if (!(n + 1))
+		return (0);
+	return (*((unsigned char *)s1) - *((unsigned char *)s2));
 }
