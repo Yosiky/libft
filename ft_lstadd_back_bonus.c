25b7686bda2e26208827f522802693a0a193b6ac
eestelle <yosiky@list.ru>
2021-10-18 17:53:49 +0300

    something

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -12,17 +12,17 @@
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *_new)
 {
 	t_list	*iter;
 
 	if (*lst == (void *)0)
-		*lst = new;
+		*lst = _new;
 	else
 	{
 		iter = *lst;
 		while (iter->next != (void *)0)
 			iter = iter->next;
-		iter->next = new;
+		iter->next = _new;
 	}
 }
--- libft.h
+++ libft.h
@@ -28,10 +28,10 @@ typedef struct s_list
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *_new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *_new);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 int		ft_isalnum(int c);
